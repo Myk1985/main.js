c8cab23ba14f265c1cc19d2d091832ce844d9d73
Myk1985 <nikolay28v85@gmail.com>
2023-05-13 10:04:55 +0300

    commit 8

--- HW4/main.js
+++ HW4/main.js
@@ -119,5 +119,12 @@ console.log(sumArr([1,2,10]));
 
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+function swap (arr,index1,index2) {
+    const value=arr[index1];
+    const value1=arr[index2];
+   arr[index1]=value1;
+   arr[index2]=value;
+}
+   (swap([11,22,33,44],0,1))
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
