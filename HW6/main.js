f77972b0a911ad67df06f3bd9ab79c2ab489a258
Myk1985 <nikolay28v85@gmail.com>
2023-05-20 02:28:19 +0300

    commit end

--- HW6/main.js
+++ HW6/main.js
@@ -83,21 +83,88 @@ console.log(mapElement)
 //     описати колоду карт (від 6 до туза без джокерів)
 
 
+let cards = [
+    { cardSuit: 'spade', value: '6', color: 'black' },
+    { cardSuit: 'diamond', value: '6', color: 'red' },
+    { cardSuit: 'heart', value: '6', color: 'red' },
+    { cardSuit: 'clubs', value: '6', color: 'black' },
+    { cardSuit: 'spade', value: '7', color: 'black' },
+    { cardSuit: 'diamond', value: '7', color: 'red' },
+    { cardSuit: 'heart', value: '7', color: 'red' },
+    { cardSuit: 'clubs', value: '7', color: 'black' },
+    { cardSuit: 'spade', value: '8', color: 'black' },
+    { cardSuit: 'diamond', value: '8', color: 'red' },
+    { cardSuit: 'heart', value: '8', color: 'red' },
+    { cardSuit: 'clubs', value: '8', color: 'black' },
+    { cardSuit: 'spade', value: '9', color: 'black' },
+    { cardSuit: 'diamond', value: '9', color: 'red' },
+    { cardSuit: 'heart', value: '9', color: 'red' },
+    { cardSuit: 'clubs', value: '9', color: 'black' },
+    { cardSuit: 'spade', value: '10', color: 'black' },
+    { cardSuit: 'diamond', value: '10', color: 'red' },
+    { cardSuit: 'heart', value: '10', color: 'red' },
+    { cardSuit: 'clubs', value: '10', color: 'black' },
+    { cardSuit: 'spade', value: 'jack', color: 'black' },
+    { cardSuit: 'diamond', value: 'jack', color: 'red' },
+    { cardSuit: 'heart', value: 'jack', color: 'red' },
+    { cardSuit: 'clubs', value: 'jack', color: 'black' },
+    { cardSuit: 'spade', value: 'queen', color: 'black' },
+    { cardSuit: 'diamond', value: 'queen', color: 'red' },
+    { cardSuit: 'heart', value: 'queen', color: 'red' },
+    { cardSuit: 'clubs', value: 'queen', color: 'black' },
+    { cardSuit: 'spade', value: 'king', color: 'black' },
+    { cardSuit: 'diamond', value: 'king', color: 'red' },
+    { cardSuit: 'heart', value: 'king', color: 'red' },
+    { cardSuit: 'clubs', value: 'king', color: 'black' },
+    { cardSuit: 'spade', value: 'ace', color: 'black' },
+    { cardSuit: 'diamond', value: 'ace', color: 'red' },
+    { cardSuit: 'heart', value: 'ace', color: 'red' },
+    { cardSuit: 'clubs', value: 'ace', color: 'black' },
+];
+
 // - знайти піковий туз
+let aBlack= cards.find((card)=>card.cardSuit===`spade` && card.value === `ace`);
+console.log(aBlack)
+
+
 // - всі шістки
+let sixCard = cards.filter((six)=>six.value===`6`);
+console.log(sixCard)
 // - всі червоні карти
+let red=cards.filter((redcard)=>redcard.color===`red`);
+console.log(red)
 // - всі буби
+let diamontCard = cards.filter((diamnd)=>diamnd.cardSuit===`diamond`);
+console.log(diamontCard)
 // - всі трефи від 9 та більше
-//
-// {
-//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
-//         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
-//     color:'', // 'red','black'
-// }
+let clubsCard=cards.filter((clubs)=>[`9`,`10`,`jack`,`queen`,`king`,`ace`].includes(clubs.value) && clubs.cardSuit===`clubs`);
+console.log(clubsCard)
+
 //
 // =========================
 //
 //     Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
+let bloc = cards.reduce((acc,card)=>{
+    switch (card.cardSuit ){
+        case `spade`:
+            acc.spades.push(card);
+            break;
+        case `diamond`:
+        acc.diamonds.push(card);
+        break;
+        case `heart`:
+            acc.hearts.push(card);
+            break;
+        case `clubs`:
+            acc.clubs.push(card);
+            break;}
+    return acc;
+
+},{spades:[],diamonds:[],hearts:[],clubs:[]})
+console.log(bloc);
+
+
+
 // {
 //     spades:[],
 //         diamonds:[],
@@ -105,6 +172,86 @@ console.log(mapElement)
 //     clubs:[]
 // }
 // =========================
+
+
+
 //     взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+
+
+
 // --написати пошук всіх об'єктів, в який в modules є sass
+let sassObj=coursesArray.filter((moD)=>moD.modules.includes(`sass`));
+console.log(sassObj)
 // --написати пошук всіх об'єктів, в який в modules є docker
+let sassObj1=coursesArray.filter((moD)=>moD.modules.includes(`docker`));
+console.log(sassObj1)
