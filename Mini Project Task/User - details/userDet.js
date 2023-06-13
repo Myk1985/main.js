9caa8d0f92e72aab17c0e9afce7927acf56328ca
Myk1985 <nikolay28v85@gmail.com>
2023-06-14 00:51:18 +0300

    commit MP2

--- Mini Project Task/User - details/userDet.js
+++ Mini Project Task/User - details/userDet.js
@@ -7,7 +7,6 @@ const userId = new URL(location.href).searchParams.get('userId');
 fetch(`http://jsonplaceholder.typicode.com/users/${userId}`)
     .then((res) => res.json())
     .then((user) => {
-        console.log(user)
         const block = document.getElementsByClassName('wrap')[0];
         const ul = document.createElement('ul');
         recursiveBuild(user, ul);
@@ -39,8 +38,32 @@ function recursiveBuild(object, parent) {
 
 // 5 Додати кнопку "post of current user", при кліку на яку, з'являються title всіх постів поточного юзера
 // (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
-//     6 Каждому посту додати кнопку/посилання, при кліку на яку відбувається перехід на сторінку post-details.html, котра має детальну інфу про поточний пост.
+const Button = document.createElement('button');
+Button.textContent = "post of current user";
+document.body.appendChild(Button);
 
+Button.addEventListener('click', () => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
+        .then(response => response.json())
+        .then(posts => {
+            const postsContainer = document.getElementById('container');
 
+            posts.forEach(post => {
+                const postTitle = document.createElement('p');
+                postTitle.textContent = post.title;
+                // 6 Каждому посту додати кнопку/посилання, при кліку на яку відбувається перехід на сторінку post-details.html, котра має детальну інфу про поточний пост.
+                const postLink = document.createElement('a');
+                postLink.innerText = 'Детальна інфо';
+                postLink.href = `../User post/user-posts.html?postId=${post.id}`;
+
+                const postWrapper = document.createElement('div');
+                postWrapper.appendChild(postTitle);
+                postWrapper.appendChild(postLink);
+
+                postsContainer.appendChild(postWrapper);
+            });
+        })
+
+});
 
 
--- Mini Project Task/User post/userPos.js
+++ Mini Project Task/User post/userPos.js
@@ -2,3 +2,35 @@
 // 7 Вивести всю, без виключення, інформацію про об'єкт post на який клікнули .
 // 8 Нижчє інформаці про пост, вивести всі коментарі поточного поста (ендпоінт  - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
 //
+
+
+const postId = new URL(location.href).searchParams.get('postId');
+
+const postDetailsBlock = document.getElementById('post-details');
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    .then(response => response.json())
+    .then(post => {
+        const postUl = document.createElement('ul');
+        for (const key in post) {
+            const li = document.createElement('li');
+            li.innerHTML = `<b>${key}:</b> ${post[key]}`;
+            postUl.appendChild(li);
+        }
+        postDetailsBlock.appendChild(postUl);
+
+
+        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+            .then(response => response.json())
+            .then(comments => {
+
+                const commentsUl = document.createElement('ul');
+                comments.forEach(comment => {
+                    const li = document.createElement('li');
+                    li.innerHTML = `<b>Name:</b> ${comment.name}<br><b>Email:</b> ${comment.email}<br><b>Body:</b> ${comment.body}`;
+                    commentsUl.appendChild(li);
+                });
+                postDetailsBlock.appendChild(commentsUl);
+            })
+
+    })
